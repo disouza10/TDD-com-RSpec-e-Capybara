833aa15730c1af1117fc64bfd4e7ba34c19fc553
Diego Souza <diegoshowza93@gmail.com>
2020-09-27 22:10:57 -0300

    adding explanation and examples for matchers

--- spec/calculator/calculator_spec.rb
+++ spec/calculator/calculator_spec.rb
@@ -1,14 +1,20 @@
 require 'calculator'
+
 describe Calculator do
-  it 'should sum 2 numbers' do
-    calc = Calculator.new
-    result = calc.sum(5, 7)
-    expect(result).to eq(12)
-  end
-  
-  it 'should sum negative numbers' do
-    calc = Calculator.new
-    result = calc.sum(5, -7)
-    expect(result).to eq(-2)
+  context '#sum' do
+    it 'with positive numbers' do
+      result = subject.sum(5, 7)
+      expect(result).to eq(12)
+    end
+    
+    it 'with negative numbers' do
+      result = subject.sum(-5, -7)
+      expect(result).to eq(-12)
+    end
+    
+    it 'with positive and negative numbers' do
+      result = subject.sum(5, -7)
+      expect(result).to eq(-2)
+    end
   end
 end
